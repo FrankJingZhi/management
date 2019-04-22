b39e11f68ab8f31d3afc8cfb95e8123f0169e54a
MariakanY <Ilubaby1314521@outlook.com>
2019-04-22 15:42:16 +0800

    把页面组件拆分成header、content、footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import store from './store'; //redux
 import { Provider } from 'react-redux'; //react-redux
+import { BrowserRouter, Route } from 'react-router-dom'; //react路由
 import Login from './pages/login';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; //react路由
-import LayoutUI from './common/layout/index';
+import Training from './pages/training/index';
 
 class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<Router>
+				<BrowserRouter>
 					<div>
 						<Route path="/" exact component={Login} />
-						<Route path="/Home" exact component={LayoutUI}/>
+						<Route path="/training" exact component={Training}/>
 					</div>
-				</Router>
+				</BrowserRouter>
 			</Provider>
 		);
 	}
--- src/pages/training/index.js
+++ src/pages/training/index.js
@@ -1,28 +1,34 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import Common from '../../common/layout/index';	//引入公共布局组件
+import { Layout } from 'antd';
+import HeaderUI from '../../common/layout/header'; //引入公共布局组件
+import FooterUI from '../../common/layout/footer'; //引入公共布局组件
+import BreadCrumbUI from '../../common/layout/breadcrumb'; //引入公共布局组件
 // import { actionCreators } from './store'; //从store文件夹引入actionCreators模块
+
+const { Content } = Layout;
 
 class Training extends PureComponent {
 	render() {
-		const training = (
-			<div>这是training页面</div>
-		);
 		return (
-			<Common 
-				content = {training}
-			/>
-			// <div>这是training页面</div>
+			<Layout>
+				<HeaderUI />
+				<Content style={{ padding: '0 50px' }}>
+					<BreadCrumbUI />
+					<Layout style={{ padding: '24px 0', background: '#fff' }}>
+						<Content style={{ padding: '0 24px', minHeight: 280 }}>
+							
+						</Content>
+					</Layout>
+				</Content>
+				<FooterUI />
+			</Layout>
 		);
 	}
 }
 
-const mapStateToProps = (state) => ({
-	
-});
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = (dispatch) => ({
-	
-});
+const mapDispatchToProps = (dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Training);
