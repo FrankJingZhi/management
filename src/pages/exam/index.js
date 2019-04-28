a799a6a1e8e62119951e6d4a96408467964ada12
MariakanY <Ilubaby1314521@outlook.com>
2019-04-28 16:46:59 +0800

    题目组件引用

--- src/pages/exam/index.js
+++ src/pages/exam/index.js
@@ -1,17 +1,57 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import HeaderUI from '../../common/layout/header'; //引入公共布局组件
+import FooterUI from '../../common/layout/footer'; //引入公共布局组件
+import BreadCrumbUI from '../../common/layout/breadcrumb'; //引入公共布局组件
+import QuestionUI from './component/question';
+import { ExamWrapper } from './style';
+import { Pagination, Layout,Button } from 'antd';
+import { actionCreators } from './store';
+import Timer from './component/timer'
+
+const { Content } = Layout;
 
 class Exam extends PureComponent {
+	componentDidMount() {
+		this.props.getQuestion();
+	}
 	render() {
+		const { Question } = this.props;
 		return (
-			<div>考试页面</div>
+			<Layout>
+				<HeaderUI />
+				<Content style={{ padding: '0 50px' }}>
+					<BreadCrumbUI />
+					<Layout style={{ padding: '24px 0px', background: '#fff', margin:'0 113px' }}>
+						<Content style={{ padding: '0 24px', minHeight: 280 }}>
+							<Timer />
+							<ExamWrapper>
+								{Question.map((item,index) => {
+									return <QuestionUI key={index} question={item} />;
+								})}
+								<div className="justifyDiv">
+									<Pagination defaultCurrent={1} total={50} />
+									<Button type="primary">交卷</Button>
+								</div>
+							</ExamWrapper>
+						</Content>
+					</Layout>
+				</Content>
+				<FooterUI />
+			</Layout>
 		);
 	}
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+	Question: state.getIn([ 'exam', 'question' ])
+});
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+	getQuestion() {
+		dispatch(actionCreators.getQuestion());
+	}
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Exam);
