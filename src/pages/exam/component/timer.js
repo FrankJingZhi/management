fef0c8b1f7d007cf34dc896c551051c72372a18d
MariakanY <Ilubaby1314521@outlook.com>
2019-04-28 16:47:14 +0800

    定时器组件

---
+++ src/pages/exam/component/timer.js
@@ -0,0 +1,29 @@
+import React,{PureComponent} from 'react';
+import { Statistic } from 'antd';
+import {connect} from 'react-redux';
+
+const Countdown = Statistic.Countdown;
+const deadline = Date.now() + 1000 * 60 * 60 * 1; // Moment is also OK
+
+class Timer extends PureComponent{
+    render(){
+        return(
+            <Countdown 
+                title="考试剩余时间：" 
+                value={deadline} 
+                format="HH:mm:ss" 
+                // onFinish={}
+                style={{textAlign:'right'}}
+            />
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
