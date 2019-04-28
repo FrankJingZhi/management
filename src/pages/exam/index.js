be26abab13c33d9d7b02fd2e9f5b4de2172beca7
MariakanY <Ilubaby1314521@outlook.com>
2019-04-28 18:24:01 +0800

    交卷时加载提示框

--- src/pages/exam/index.js
+++ src/pages/exam/index.js
@@ -6,16 +6,48 @@ import FooterUI from '../../common/layout/footer'; //引入公共布局组件
 import BreadCrumbUI from '../../common/layout/breadcrumb'; //引入公共布局组件
 import QuestionUI from './component/question';
 import { ExamWrapper } from './style';
-import { Pagination, Layout,Button } from 'antd';
+import { Pagination, Layout, Button, Modal } from 'antd';
 import { actionCreators } from './store';
-import Timer from './component/timer'
+import Timer from './component/timer';
+import { Redirect } from 'react-router-dom'; //引入Redirect组件用于js上的页面跳转
 
 const { Content } = Layout;
+const confirm = Modal.confirm;
 
 class Exam extends PureComponent {
+
 	componentDidMount() {
 		this.props.getQuestion();
 	}
+
+	showConfirm() {
+		Modal.confirm({
+			title: '提示',
+			content: '你确定要提交试卷吗？',
+			okText: '确认',
+    		cancelText: '取消',
+			onOk() {
+				console.log('确定',window.localStorage);
+				confirm({
+					title: '提示',
+					content: '你所得的分数是：',
+					okText: '确认',
+					cancelText: '取消',
+					onOk() {
+						return <Redirect to="/training"/>;
+					},
+					onCancel(){
+						console.log('取消')
+					}
+				});
+			},
+			onCancel() {
+				console.log('取消');
+			}
+		});
+	}
+
+
 	render() {
 		const { Question } = this.props;
 		return (
@@ -23,16 +55,18 @@ class Exam extends PureComponent {
 				<HeaderUI />
 				<Content style={{ padding: '0 50px' }}>
 					<BreadCrumbUI />
-					<Layout style={{ padding: '24px 0px', background: '#fff', margin:'0 113px' }}>
+					<Layout style={{ padding: '24px 0px', background: '#fff', margin: '0 113px' }}>
 						<Content style={{ padding: '0 24px', minHeight: 280 }}>
 							<Timer />
 							<ExamWrapper>
-								{Question.map((item,index) => {
+								{Question.map((item, index) => {
 									return <QuestionUI key={index} question={item} />;
 								})}
 								<div className="justifyDiv">
 									<Pagination defaultCurrent={1} total={50} />
-									<Button type="primary">交卷</Button>
+									<Button type="primary" onClick={() => this.showConfirm()}>
+										交卷
+									</Button>
 								</div>
 							</ExamWrapper>
 						</Content>
@@ -45,13 +79,13 @@ class Exam extends PureComponent {
 }
 
 const mapStateToProps = (state) => ({
-	Question: state.getIn([ 'exam', 'question' ])
+	Question: state.getIn([ 'exam', 'question' ]),
 });
 
 const mapDispatchToProps = (dispatch) => ({
 	getQuestion() {
 		dispatch(actionCreators.getQuestion());
-	}
+	},
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Exam);
