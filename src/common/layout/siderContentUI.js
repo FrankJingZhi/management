7c3d29010fd1b7ab6e3cde7517d558a5fb54d42e
MariakanY <Ilubaby1314521@outlook.com>
2019-04-29 15:11:54 +0800

    侧边栏提取组件

--- src/common/layout/siderContentUI.js
+++ src/common/layout/siderContentUI.js
@@ -1,40 +1,25 @@
 import React, { PureComponent } from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu } from 'antd';
 import { connect } from 'react-redux';
-const { SubMenu } = Menu;
-const { Content, Sider } = Layout;
+const { Sider } = Layout;
 
 class SiderContentUI extends PureComponent {
 	render() {
+		const {SiderMenu} = this.props;
 		return (
-			<Layout style={{ padding: '24px 0', background: '#fff' }}>
-				<Sider width={200} style={{ background: '#fff' }}>
-					<Menu
-						mode="inline"
-						defaultSelectedKeys={[ '1' ]}
-						defaultOpenKeys={[ 'sub1' ]}
-						style={{ height: '100%' }}
-					>
-						<SubMenu
-							key="sub1"
-							title={
-								<span>
-									<Icon type="user" />subnav 1
-								</span>
-							}
-						>
-							<Menu.Item key="1">option1</Menu.Item>
-							<Menu.Item key="2">option2</Menu.Item>
-							<Menu.Item key="3">option3</Menu.Item>
-							<Menu.Item key="4">option4</Menu.Item>
-						</SubMenu>
-					</Menu>
-				</Sider>
-			</Layout>
+			<Sider width={200} style={{ background: '#fff' }}>
+				<Menu mode="inline" selectedKeys defaultSelectedKeys={[ '1' ]} style={{ height: '100%' }}>
+					{SiderMenu.map((item,index)=>{
+						return <Menu.Item key={index}>{item.get('name')}</Menu.Item>
+					})}
+				</Menu>
+			</Sider>
 		);
 	}
 }
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+	SiderMenu:state.getIn(['common','siderMenu']), //侧边栏数据
+});
 
 const mapDispatchToProps = (dispatch) => ({});
 
--- src/common/layout/store/reducer.js
+++ src/common/layout/store/reducer.js
@@ -3,7 +3,7 @@
  * @LastEditors: Do not edit
  * @description: 图书馆管理员的笔记本--
  * @since: 2019-04-18 17:36:48
- * @lastTime: 2019-04-19 10:20:10
+ * @lastTime: 2019-04-29 12:02:58
  */
 import { fromJS } from 'immutable';
 import * as containts from './containts';
@@ -16,14 +16,19 @@ import * as containts from './containts';
 const defaultState = fromJS({
 	// 头部标签
 	headerItem:[
-		{id:'headerItem1',name:'训练'},
-		{id:'headerItem2',name:'测试'},
-		{id:'headerItem3',name:'管理'}
+		{id:'header_1',name:'训练'},
+		{id:'header_2',name:'测试'},
+		{id:'header_3',name:'管理'}
 	],
 	// 头部个人中心
 	dropDownMenu:[
-		{id:'dropDownMenu1',name:'个人中心',url:''},
-		{id:'dropDownMenu2',name:'退出',url:''},
+		{id:'dropDown_1',name:'个人中心',url:''},
+		{id:'dropDown_2',name:'退出',url:''},
+	],
+	siderMenu:[
+		{id:'sider_1',name:'用户管理'},
+		{id:'sider_2',name:'试卷管理'},
+		{id:'sider_3',name:'题目管理'},
 	]
 });
 
