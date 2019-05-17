5da4281b0eb9d908c73b7f6e46dc9cc0390611d7
15666795711 <Ilubaby1314521@outlook.com>
2019-05-17 15:01:24 +0800

    person修改密码弹框组件引用

--- src/pages/person/index.js
+++ src/pages/person/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Layout } from 'antd';
 import UpdateForm from './components/updateForm';
+import UpdateModal from './components/updateModal';
 import { actionCreators } from './store'; //从store文件夹引入actionCreators模块
 
 const { Content } = Layout;
@@ -12,7 +13,7 @@ class Person extends PureComponent {
 		return (
 			<Content style={{ padding: '0 24px', minHeight: 280 }}>
 				<UpdateForm />
-				
+				<UpdateModal />
 			</Content>
 		);
 	}
